d62cfff5418cb194fe3f9c6a54070b4916c17949
DESKTOP-P23IA6M\ukone <ucone92@gmail.com>
2017-04-18 11:40:44 +0300

    Changed colors of scrollbar and button on apointment page

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -4,7 +4,7 @@
 
     // custom scrollbar
 
-    $("html").niceScroll({styler:"fb",cursorcolor:"#fe4630", cursorwidth: '6', cursorborderradius: '10px', background: '#FFFFFF', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
+    $("html").niceScroll({styler:"fb",cursorcolor:"#2E64FE", cursorwidth: '6', cursorborderradius: '10px', background: '#FFFFFF', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
 
     $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"#fe4630", cursorwidth: '6', cursorborderradius: '0',autohidemode: 'false', background: '#FFFFFF', spacebarenabled:false, cursorborder: '0'});
 
